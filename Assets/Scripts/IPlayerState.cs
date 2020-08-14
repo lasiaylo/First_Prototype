789d0691e5dc9dabeb55e4e02c9bd75259e73031
Lasia <lasia.lo@duke.edu>
2020-08-14 18:18:10 -0400

    Added PlayerState Interface

---
+++ Assets/Scripts/IPlayerState.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IPlayerState {
+    void Start(PlayerController player);
+
+    void Update(PlayerController player);
+
+    void FixedUpdate(PlayerController player);
+
+    void Exit(PlayerController player);
+}
---
+++ Assets/Scripts/IdleState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : IPlayerState {
+    public void Start(PlayerController player) {
+        throw new System.NotImplementedException();
+
+    }
+
+    public void Update(PlayerController player) {
+        
+        
+    }
+
+    public void FixedUpdate(PlayerController player) {
+        throw new System.NotImplementedException();
+    }
+
+    public void Exit(PlayerController player) {
+        throw new System.NotImplementedException();
+    }
+}
