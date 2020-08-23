884fb6ffec28164472a7846e2dbf30333f5df311
Lasia <lasia.lo@duke.edu>
2020-08-23 19:31:56 -0400

    Vector Extensions

--- Assets/Scripts/Util/Extension.cs
+++ Assets/Scripts/Util/Extension.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 namespace Util {
     public static class Extension {
-        private static float _tolerance = 0.01f; 
         public static T Clamp<T>(this T val, T min, T max) where T : IComparable<T> {
             if (val.CompareTo(min) < 0) return min;
             if (val.CompareTo(max) > 0) return max;
@@ -17,18 +16,6 @@ namespace Util {
 
         public static T ClampMin<T>(this T val, T min) where T : IComparable<T> {
             return val.CompareTo(min) < 0 ? min : val;
-        }
-
-        public static bool IsZero(this Vector3 val) {
-            return Math.Abs(val.magnitude) < _tolerance;
-        }
-
-        public static bool IsZero(this Vector2 val) {
-            return Math.Abs(val.magnitude) < _tolerance;
-        }
-
-        public static Vector2 Xz(this Vector3 val) {
-            return new Vector2(val.x, val.z);
         }
     }
 }
