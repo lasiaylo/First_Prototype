d97ee22a77427ce83784e2c3e33b40b6d0b9930e
Lasia Lo <lasiaylo@gmail.com>
2020-08-29 03:09:23 -0400

    FP-72 (Not Tested) Implement rotation

---
+++ Assets/Scripts/Translate/Rotation/Turn.cs
@@ -0,0 +1,17 @@
+﻿using ScriptableObjects.Prototypes;
+using Translate.Movement;
+using UnityEngine;
+using Util.Attributes;
+
+namespace Translate.Rotation {
+    public class Turn : Mod<Quaternion> {
+        [Expandable] public TurnTraits traits; 
+        public override Quaternion Modify(Quaternion val) {
+            return Quaternion.RotateTowards(
+                val, 
+                Quaternion.LookRotation(traits.Direction), 
+                traits.TurnSpeed * Time.deltaTime
+            );
+        }
+    }
+}
