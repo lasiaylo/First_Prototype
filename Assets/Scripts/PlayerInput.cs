cd3bbbb265125f03d49b8472a735a6e4e161fb40
Lasia <lasiaylo@gmail.com>
2020-11-06 11:14:03 -0500

    Animate Player

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -5,17 +5,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Utilities;
-using Object = UnityEngine.Object;
 
-public class PlayerInput : IInputActionCollection, IDisposable {
-    // Gameplay
-    private readonly InputActionMap m_Gameplay;
-    private readonly InputAction m_Gameplay_Jump;
-    private readonly InputAction m_Gameplay_Movement;
-    private IGameplayActions m_GameplayActionsCallbackInterface;
-    private int m_KeyboardSchemeIndex = -1;
-
-    public PlayerInput() {
+public class @PlayerInput : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @PlayerInput()
+    {
         asset = InputActionAsset.FromJson(@"{
     ""name"": ""PlayerInput"",
     ""maps"": [
@@ -119,108 +114,106 @@ public class PlayerInput : IInputActionCollection, IDisposable {
     ]
 }");
         // Gameplay
-        m_Gameplay = asset.FindActionMap("Gameplay", true);
-        m_Gameplay_Movement = m_Gameplay.FindAction("Movement", true);
-        m_Gameplay_Jump = m_Gameplay.FindAction("Jump", true);
+        m_Gameplay = asset.FindActionMap("Gameplay", throwIfNotFound: true);
+        m_Gameplay_Movement = m_Gameplay.FindAction("Movement", throwIfNotFound: true);
+        m_Gameplay_Jump = m_Gameplay.FindAction("Jump", throwIfNotFound: true);
     }
 
-    public InputActionAsset asset { get; }
-    public GameplayActions Gameplay => new GameplayActions(this);
-
-    public InputControlScheme KeyboardScheme {
-        get {
-            if (m_KeyboardSchemeIndex == -1) m_KeyboardSchemeIndex = asset.FindControlSchemeIndex("Keyboard");
-            return asset.controlSchemes[m_KeyboardSchemeIndex];
-        }
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
     }
 
-    public void Dispose() {
-        Object.Destroy(asset);
-    }
-
-    public InputBinding? bindingMask {
+    public InputBinding? bindingMask
+    {
         get => asset.bindingMask;
         set => asset.bindingMask = value;
     }
 
-    public ReadOnlyArray<InputDevice>? devices {
+    public ReadOnlyArray<InputDevice>? devices
+    {
         get => asset.devices;
         set => asset.devices = value;
     }
 
     public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
 
-    public bool Contains(InputAction action) {
+    public bool Contains(InputAction action)
+    {
         return asset.Contains(action);
     }
 
-    public IEnumerator<InputAction> GetEnumerator() {
+    public IEnumerator<InputAction> GetEnumerator()
+    {
         return asset.GetEnumerator();
     }
 
-    IEnumerator IEnumerable.GetEnumerator() {
+    IEnumerator IEnumerable.GetEnumerator()
+    {
         return GetEnumerator();
     }
 
-    public void Enable() {
+    public void Enable()
+    {
         asset.Enable();
     }
 
-    public void Disable() {
+    public void Disable()
+    {
         asset.Disable();
     }
 
-    public struct GameplayActions {
-        private readonly PlayerInput m_Wrapper;
-
-        public GameplayActions(PlayerInput wrapper) {
-            m_Wrapper = wrapper;
-        }
-
-        public InputAction Movement => m_Wrapper.m_Gameplay_Movement;
-        public InputAction Jump => m_Wrapper.m_Gameplay_Jump;
-
-        public InputActionMap Get() {
-            return m_Wrapper.m_Gameplay;
-        }
-
-        public void Enable() {
-            Get().Enable();
-        }
-
-        public void Disable() {
-            Get().Disable();
-        }
-
+    // Gameplay
+    private readonly InputActionMap m_Gameplay;
+    private IGameplayActions m_GameplayActionsCallbackInterface;
+    private readonly InputAction m_Gameplay_Movement;
+    private readonly InputAction m_Gameplay_Jump;
+    public struct GameplayActions
+    {
+        private @PlayerInput m_Wrapper;
+        public GameplayActions(@PlayerInput wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Gameplay_Movement;
+        public InputAction @Jump => m_Wrapper.m_Gameplay_Jump;
+        public InputActionMap Get() { return m_Wrapper.m_Gameplay; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
         public bool enabled => Get().enabled;
-
-        public static implicit operator InputActionMap(GameplayActions set) {
-            return set.Get();
-        }
-
-        public void SetCallbacks(IGameplayActions instance) {
-            if (m_Wrapper.m_GameplayActionsCallbackInterface != null) {
-                Movement.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMovement;
-                Movement.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMovement;
-                Movement.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMovement;
-                Jump.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
-                Jump.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
-                Jump.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+        public static implicit operator InputActionMap(GameplayActions set) { return set.Get(); }
+        public void SetCallbacks(IGameplayActions instance)
+        {
+            if (m_Wrapper.m_GameplayActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnMovement;
+                @Jump.started -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                @Jump.performed -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
+                @Jump.canceled -= m_Wrapper.m_GameplayActionsCallbackInterface.OnJump;
             }
-
             m_Wrapper.m_GameplayActionsCallbackInterface = instance;
-            if (instance != null) {
-                Movement.started += instance.OnMovement;
-                Movement.performed += instance.OnMovement;
-                Movement.canceled += instance.OnMovement;
-                Jump.started += instance.OnJump;
-                Jump.performed += instance.OnJump;
-                Jump.canceled += instance.OnJump;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @Jump.started += instance.OnJump;
+                @Jump.performed += instance.OnJump;
+                @Jump.canceled += instance.OnJump;
             }
         }
     }
-
-    public interface IGameplayActions {
+    public GameplayActions @Gameplay => new GameplayActions(this);
+    private int m_KeyboardSchemeIndex = -1;
+    public InputControlScheme KeyboardScheme
+    {
+        get
+        {
+            if (m_KeyboardSchemeIndex == -1) m_KeyboardSchemeIndex = asset.FindControlSchemeIndex("Keyboard");
+            return asset.controlSchemes[m_KeyboardSchemeIndex];
+        }
+    }
+    public interface IGameplayActions
+    {
         void OnMovement(InputAction.CallbackContext context);
         void OnJump(InputAction.CallbackContext context);
     }
